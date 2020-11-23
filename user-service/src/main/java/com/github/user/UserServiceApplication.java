99a595a94568791d31d01bdd27b7f76ce7f4b45e
peach <taofengfeng>
2020-11-23 17:43:12 +0800

    feat:增加了用户微服务

---
+++ user-service/src/main/java/com/github/user/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.github.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author peach
+ * @since 2020/11/23 17:18
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
