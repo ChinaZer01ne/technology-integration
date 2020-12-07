8fe51df100d84b6e63e2d2703256ea623af25e5f
peach <taofengfeng>
2020-12-07 17:35:12 +0800

    feat:增加sentinel通用包

---
+++ common/common-sentinel/src/main/java/com/github/sentinel/SentinelConfig.java
@@ -0,0 +1,57 @@
+package com.github.sentinel;
+
+import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+
+/**
+ * @author peach
+ * @since 2020/12/7 14:04
+ */
+@Configuration
+public class SentinelConfig {
+
+    @ConditionalOnClass(name = {"org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext"})
+    @Bean
+    public BlockRequestHandler blockRequestHandler() {
+        return new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange exchange,
+                                                      Throwable t) {
+                return ServerResponse.status(444).contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(JSONObject.toJSONString(com.github.common.core.ServerResponse.fail(t.getMessage()))));
+            }
+        };
+    }
+
+    @ConditionalOnClass(name = {"org.springframework.web.context.WebApplicationContext"})
+    @Bean
+    public BlockExceptionHandler blockExceptionHandler() {
+        return new BlockExceptionHandler() {
+            @Override
+            public void handle(HttpServletRequest request, HttpServletResponse response, BlockException e) throws Exception {
+                //logger.error("sentinel 降级 资源名称{}", e.getRule().getResource(), e);
+                response.setContentType(MediaType.APPLICATION_JSON.getType());
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                response.getWriter().print(JSONObject.toJSONString(com.github.common.core.ServerResponse.fail(e.getMessage())));
+            }
+        };
+    }
+}
