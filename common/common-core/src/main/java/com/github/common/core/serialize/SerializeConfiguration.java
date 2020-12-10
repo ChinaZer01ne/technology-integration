e2eef293d5e349a80897150b890cec505856eff3
peach <taofengfeng>
2020-12-10 14:07:50 +0800

    feat:序列化自动配置

--- common/common-core/src/main/java/com/github/common/core/serialize/SerializeConfiguration.java
+++ common/common-core/src/main/java/com/github/common/core/serialize/SerializeConfiguration.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +19,14 @@ import java.time.format.DateTimeFormatter;
  * 配置全局序列化的两种方式：
  *  1、配置Jackson2ObjectMapperBuilderCustomizer
  *  2、配置ObjectMapper
+ *      2.1、如果需要使用自动配置的方式，应该使用@AutoConfigureBefore({JacksonAutoConfiguration.class})，让ObjectMapper在JacksonAutoConfiguration之前注入进来
  *
  * 全局序列话的东西不一定每个微服务都要使用，独立于common包会更好。
+ *
  * @author peach
  * @since 2020/12/1 15:40
  */
+@AutoConfigureBefore({JacksonAutoConfiguration.class})
 @Configuration
 public class SerializeConfiguration {
     ///**
