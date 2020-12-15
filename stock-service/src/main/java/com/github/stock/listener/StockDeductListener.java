a174650e8dafbcbbbb9cc2c0206fb802aca26e64
peach <taofengfeng>
2020-12-15 17:53:56 +0800

    feat:RocketMQ整合

--- stock-service/src/main/java/com/github/stock/listener/StockDeductListener.java
+++ stock-service/src/main/java/com/github/stock/listener/StockDeductListener.java
@@ -1,23 +1,26 @@
 package com.github.stock.listener;
 import com.github.internal.api.order.dto.OrderDTO;
 import com.github.stock.service.StockService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.annotation.RocketMQTransactionListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Zer01ne
  * @since 2020/12/13 16:38
  */
-@RocketMQMessageListener(consumerGroup = "stock-service", topic = "OrderPaidSuccess")
-public class StockDeductListener implements RocketMQListener {
+@Slf4j
+//@Component
+@RocketMQMessageListener(consumerGroup = "stock-consumer", topic = "OrderPaidSuccess")
+public class StockDeductListener implements RocketMQListener<OrderDTO> {
 
     @Autowired
     private StockService stockService;
     @Override
-    public void onMessage(Object message) {
-        OrderDTO order = (OrderDTO) message;
+    public void onMessage(OrderDTO order) {
         stockService.deduct(order);
     }
 }
--- stock-service/src/main/java/com/github/stock/listener/StockLockListener.java
+++ stock-service/src/main/java/com/github/stock/listener/StockLockListener.java
@@ -6,22 +6,23 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.annotation.RocketMQTransactionListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Zer01ne
  * @since 2020/12/13 16:38
  */
 @Slf4j
-@RocketMQMessageListener(consumerGroup = "stock-service", topic = "OrderCreated")
-public class StockLockListener implements RocketMQListener {
+@Component
+@RocketMQMessageListener(consumerGroup = "stock-consumer", topic = "OrderCreated")
+public class StockLockListener implements RocketMQListener<OrderDTO> {
 
     @Autowired
     private StockService stockService;
 
     @Override
-    public void onMessage(Object message) {
+    public void onMessage(OrderDTO order) {
         log.info("锁定库存");
-        OrderDTO order = (OrderDTO) message;
         stockService.lock(order);
     }
 }
