ed68e8dea2ea727ce2cf68a3332c59937df57bc8
Zer01ne <Zer01ne>
2021-12-02 00:17:20 +0800

    feat:修改了pom依赖

--- order-service/src/main/java/com/github/order/config/Config.java
+++ order-service/src/main/java/com/github/order/config/Config.java
@@ -1,10 +1,16 @@
 package com.github.order.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ConditionalOnBean(value = String.class)
 public class Config {
-
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
